d23704b7e7d76f67f78a58e1402406d8fa5779a5
popkirby <popkirby@gmail.com>
2015-10-14 21:01:31 +0900

    add test cases cheking warnings

--- test/cases/decorator_test.js
+++ test/cases/decorator_test.js
@@ -1,8 +1,19 @@
-import { propTypes, defaultProps, contextTypes } from '../../lib/index';
+import { propTypes, defaultProps, contextTypes, childContextTypes } from '../../lib/index';
 import React from 'react';
 import assert from 'power-assert';
+import ReactDOMServer from 'react-dom/server';
+import sinon from 'sinon';
 
 describe('decorator tests', () => {
+
+  beforeEach(() => {
+    // TODO: React < v0.14.0 uses console.warn
+    sinon.spy(console, 'error');
+  });
+
+  afterEach(() => {
+    console.error.restore();
+  });
 
   describe('@propTypes', () => {
     it('should set Component.propTypes', () => {
@@ -19,6 +30,11 @@ describe('decorator tests', () => {
 
       assert(Component.propTypes.foo === React.PropTypes.string);
       assert(Component.propTypes.bar === React.PropTypes.any);
+
+      const aComponent = <Component foo={1} />;
+
+      assert(console.error.calledOnce);
+
     });
   });
 
@@ -37,6 +53,12 @@ describe('decorator tests', () => {
 
       assert(Component.defaultProps.foo === "defaultValue");
       assert(Component.defaultProps.bar === 100);
+
+      const aComponent = <Component />;
+
+      assert(aComponent.props.foo === "defaultValue");
+      assert(aComponent.props.bar === 100);
+
     });
   });
 
@@ -44,15 +66,33 @@ describe('decorator tests', () => {
     it('should set Component.contextTypes', () => {
 
       @contextTypes({
-        "foo": React.PropTypes.func
+        "foo": React.PropTypes.number.isRequired
       })
       class Component extends React.Component {
         render() {
-          return <div></div>;
+          return <div>{this.context.foo}</div>;
         }
       }
 
-      assert(Component.contextTypes.foo === React.PropTypes.func);
+      @childContextTypes({
+        "foo": React.PropTypes.any.isRequired
+      })
+      class Parent extends React.Component {
+        getChildContext() {
+          return { foo: 'hoge' };
+        }
+
+        render() {
+          return <Component />;
+        }
+      }
+
+      assert(Component.contextTypes.foo === React.PropTypes.number.isRequired);
+      assert(Parent.childContextTypes.foo === React.PropTypes.any.isRequired);
+
+      const aComponent = ReactDOMServer.renderToStaticMarkup(<Parent />);
+
+      assert(console.error.calledOnce);
     });
   });
 
